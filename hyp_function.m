f7ff65fffc5f233785fe3daa7acf8a4d81471574
krishanuphy <130661078+krishanuphy@users.noreply.github.com>
2023-10-20 01:03:22 +0530

    Add files via upload

---
+++ hyp_function.m
@@ -0,0 +1,86 @@
+%Plant Biology
+
+% p_self included here only
+function [diff] = model(t,dp,p_b,m_b,k_r,d_e,m_c,p_cl,p_cd,d_c...
+    ,m_p,p_p,p_pe,d_p,k_pc,d_pb,p_g,k_g,p_gp,p_ge,p_gb,p_gh,p_hc,p_f,p_fp,d_f,k_0,p_0,D,p_t1,k_01,m_f, ...
+    d_ec) 
+
+
+B = dp(1);     %phyB 
+E = dp(2);     %ELF3 
+C = dp(3);     %Cop1 
+P = dp(4);     %PIF4 
+G = dp(5);     %Hypocotyl Growth
+F = dp(6);     %GUS activity
+
+%Defining Parameters
+
+m_e = 1;       
+p_e1 = 108;
+p_e2 = 39.8;
+t0 = mod(t,24);
+t1 = t0 - D;
+t2 = t0 - 24;
+k0 = 5;
+
+if t0 >= D      %Condition for day cycle
+    L = 0;
+end
+
+if t0 < D
+    L= 1;
+end
+
+if 0 < D < 24   %From equation 2
+
+p_e = m_e*p_e1 - p_e2*(-1+(2/(1+exp(-k0*t0)))-(2/(1+exp(-k0*t1)))+...
+     (2/(1+exp(-k0*t2))));
+end
+
+if D == 0       %From equation 2
+
+    p_e = m_e*p_e1 + p_e2;
+
+end
+
+if D == 24      %From equation 2
+
+    p_e = m_e*p_e1 - p_e2;
+
+end
+
+
+
+
+if P >= p_t1    %Introducing Threshold
+    
+    p_s2 = m_p*(p_p/(1+p_pe*E+p_0*P));
+    
+    p_s1 = k_0 + (p_f)/(1+p_fp*P) ;
+
+    
+end
+
+if P < p_t1     %Introducing Threshold
+    
+
+    p_s2 = m_p*(p_p/(1+p_pe*E));  %%%%% activator
+    
+    p_s1 = m_f*k_01 ; %%%% 
+
+    
+end
+ 
+
+
+% Defining Equation 1
+   
+diff = [p_b*L*(m_b - B) - k_r*B
+       p_e - d_ec*E*C - d_e*E
+       m_c*(p_cl*L + p_cd*(1-L))- d_c*C
+       p_s2 - (d_p/(1+k_pc*C))*P - d_pb*B*P
+       p_g + k_g *((p_gp*P)/(1+p_ge*E+p_gb*B+((p_gh)/(1+p_hc*C))))
+       p_s1 - d_f*F];
+
+  
+end
---
+++ main_code.m
@@ -0,0 +1,151 @@
+clear all;
+a1 = [];
+i = 1;
+d1 = [];
+
+
+    
+domain = [0 120];
+
+xmax = 120;
+xmin = 0;
+
+color = ["#000000" "#FF0000" "#0072BD" "#EDB120"];
+
+
+p_b = 10.0;
+m_b = 1;
+k_r = 0.232;
+d_e = 27.2;
+m_c =  1;
+p_cl = 1.00;
+p_cd = 112;
+d_c  = 1.79;
+p_p  = 1;
+p_pe = 0.332;
+d_p = 4.91;
+k_pc = 34.3;
+d_pb = 0.313;
+p_g = 0.009;
+k_g = 0.113;
+p_gp = 2.93;
+p_ge = 0.465;
+p_gb = 10.7;
+p_gh = 0; 
+p_hc = 0.180;
+p_f = 450; 
+p_fp = 22;
+d_f = 0.0009;
+k_0 = 10;
+p_0 = 25;
+k_01 = 10;
+m_f = 1;
+d_ec = 0.01;
+ 
+p_th = [0.2 0.2 0.2 0.2];       %Threshold Value for four genotype
+
+
+
+
+
+
+
+
+
+%Experimental Data
+
+exp_D = [8 16];                         %Day legth
+exp_hyp = [4.2228 2.269105263];         %Hypocotyle length for wildtype(WT)
+exp_dev = [0.278813577 0.238650813];    %standard devation of the experimental measurement of hypocotyl for WT 
+ 
+exp_hyp_mut = [2.64675 1.5195];              %Hypocotyl legth of pif4-101
+exp_hyp_mut_dev = [0.250230815 0.221709744]; %standard devation of the experimental measurement of hypocotyl for pif4-101 
+
+exp_hyp_oe1 = [8.164526316 3.3828];          %Hypocotyl legth of PIF4-OE1
+exp_hyp_oe1_dev = [0.705548673 0.341386739]; %standard devation of the experimental measurement of hypocotyl for PIF4-OE1  
+
+exp_hyp_oe2 = [10.07135 6.3037];             %Hypocotyl legth of PIF4-OE2
+exp_hyp_oe2_dev = [0.820796101 0.544857312]; %standard devation of the experimental measurement of hypocotyl for PIF4-OE2
+
+m=0;
+for m_p = [1 0.12 4 8.5]
+
+    p_t1 = p_th(i)
+    i = i+1;
+    j=0;
+for D = 1:24
+
+j = j+1;
+%initial condition 
+
+tc1 = 0;   %phyB initial condition
+tc2 = 0;   %ELF3 initial condition        
+tc3 = 0;   %COP1 initial condition        
+tc4 = 0;   
+tc5 = 0;
+tc6 = 0;
+
+tc = [tc1 tc2 tc3 tc4 tc5 tc6];
+
+[IVSOL, DVSOL] = ode45(@(t,dp) hyp_function(t,dp,p_b,m_b,k_r,d_e,m_c,p_cl,p_cd,d_c...
+    ,m_p,p_p,p_pe,d_p,k_pc,d_pb,p_g,k_g,p_gp,p_ge,p_gb,p_gh,p_hc,p_f,p_fp,d_f,k_0,p_0,D,p_t1,k_01,m_f, ...
+    d_ec),domain,tc);    
+                               
+
+
+
+
+[Row,Col] = size(DVSOL);
+
+g_end = DVSOL(Row,5);
+
+g_end_data(j) = g_end;
+day_length(j) = D;
+
+
+
+end
+m = m+1;
+plot(day_length,g_end_data,'-','Color',color(m),'LineWidth',5)
+hold on;
+
+
+end
+
+
+e = errorbar(exp_D,exp_hyp,exp_dev,"o",'LineWidth',1);
+e.Marker = 'o';
+e.MarkerSize = 5;
+e.Color = "#000000";
+e.CapSize = 10;
+
+e = errorbar(exp_D,exp_hyp_mut,exp_hyp_mut_dev,"o",'LineWidth',1);
+e.Marker = 'o';
+e.MarkerSize = 5;
+e.Color = "#FF0000";
+e.CapSize = 10;
+
+e = errorbar(exp_D,exp_hyp_oe1,exp_hyp_oe1_dev,"o",'LineWidth',1);
+e.Marker = 'o';
+e.MarkerSize = 5;
+e.Color = "#0072BD";
+e.CapSize = 10;
+
+e = errorbar(exp_D,exp_hyp_oe2,exp_hyp_oe2_dev,"o",'LineWidth',1);
+e.Marker = 'o';
+e.MarkerSize = 5;
+e.Color = "#EDB120";
+e.CapSize = 10;
+
+xlabel('Day Length(Hour)','Fontsize',14)
+ylabel('Growth','Fontsize',14)
+xlim([1 24])
+
+txt = ['Hypocotyl Length vs Day Length Graph,Model Code No - 18a, Mutp - 0.12,1,4,8.5 and' ...
+    ' threshold value = 0.15(p_t_1) and P_s_e_l_f = 25'];
+sgtitle(txt);
+
+
+
+
+
